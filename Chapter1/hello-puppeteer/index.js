00139c8c01d45c12e1fecfb6fe31f33d2098452b
Dario Kondratiuk <uitestingwithpuppeteer@gmail.com>
2021-02-07 12:58:32 -0300

    Update chapter 1

--- Chapter1/hello-puppeteer/index.js
+++ Chapter1/hello-puppeteer/index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async function() {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://en.wikipedia.org/wiki/%22Hello,_World!%22_program');
     await page.screenshot({ path: './screenshot.png'});
