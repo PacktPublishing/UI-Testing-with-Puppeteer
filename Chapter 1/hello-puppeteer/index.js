20debb33f43f877e98c36c3481b7d91417adc2f4
Dario Kondratiuk <uitestingwithpuppeteer@gmail.com>
2021-02-10 09:37:07 -0300

    Remove wrong files

--- Chapter 1/hello-puppeteer/index.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async function() {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://en.wikipedia.org/wiki/%22Hello,_World!%22_program');
-    await page.screenshot({ path: './screenshot.png'});
-    browser.close();
-})();
