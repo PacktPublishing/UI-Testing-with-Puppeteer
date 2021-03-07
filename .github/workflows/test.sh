4f5e6f5d092d1cfb9eaa59a9bf59fbc6a83a09f8
Darío Kondratiuk <dariokondratiuk@gmail.com>
2021-03-07 15:34:18 -0300

    Test all chapters

---
+++ .github/workflows/test.sh
@@ -0,0 +1,16 @@
+cd ./Chapter3/vuejs-firebase-shopping-cart
+npm run serve & npx wait-on http://localhost:8080
+cd ..
+npm test
+cd ../Chapter4
+npm test
+cd ../Chapter5
+npm test
+cd ../Chapter6
+npm test
+cd ../Chapter7
+npm test
+cd ../Chapter8
+npm test
+cd ../Chapter10
+npm test
