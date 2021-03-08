021ec44e644ea17379b1478ff6f37596928c24e5
Dario Kondratiuk <dariokondratiuk@gmail.com>
2021-03-07 21:49:40 -0300

    Improve tests

--- Chapter6/test/weather.tests.js
+++ Chapter6/test/weather.tests.js
@@ -21,7 +21,7 @@ describe('Weather Channel Test', () => {
 
     it('Should send geolocation', async() => {
         const promise = page.waitForRequest(r => r.url().includes('redux-dal'));
-        await page.goto('https://weather.com/');
+        await page.goto('https://weather.com/', { waitUntil: 'domcontentloaded'});
         const request = await promise;
         const json = JSON.parse(request.postData());
         expect(json[0].params.geocode).not.be.empty;
