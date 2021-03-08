b5caf5ca31de4a3480b9ab35a5c8e3c8ce3c42a3
Dario Kondratiuk <dariokondratiuk@gmail.com>
2021-03-07 21:38:02 -0300

    sync packpub tests

--- Chapter7/test/packpub.tests.js
+++ Chapter7/test/packpub.tests.js
@@ -29,7 +29,7 @@ describe('Packpub search Test', () => {
         await browser.close();
     })
 
-    it('Should send geolocation', async() => {
+    it('Should send geolocation', async () => {
         page.goto('https://www.packtpub.com/');
         const search = await page.waitForSelector('#search');
         await search.type('Puppeteer');
@@ -44,17 +44,17 @@ describe('Packpub search Test', () => {
 
     });
 
-    it('Should update the cart number', async() => {
+    it('Should update the cart number', async () => {
         await page.goto('https://www.packtpub.com/tech/javascript/');
-        const cookieLink = await page.waitForSelector('.accept_all', { timeout: 1000}).catch(e => e);
+        const cookieLink = await page.waitForSelector('.accept_all', { timeout: 1000 }).catch(e => e);
 
         if (cookieLink) {
-          await cookieLink.click().catch({});
+            await cookieLink.click().catch({});
         }
 
         await Promise.all([
-          page.waitForSelector('.counter.qty.empty'),
-          page.waitForSelector('.add-to-cart')
+            page.waitForSelector('.counter.qty.empty'),
+            page.waitForSelector('.add-to-cart')
         ]);
 
         const cartIsOnePromise = page.waitForFunction(() => document.querySelector('.counter.qty .counter-number').innerText.trim() === '1');
@@ -63,35 +63,35 @@ describe('Packpub search Test', () => {
         await addToCartButtons[0].click();
 
         await page.waitForFunction(async () => {
-          const element = document.querySelector('.block-minicart');
-          let currentHeight = element.getBoundingClientRect().height;
-          let stopMovingCounter = 0;
+            const element = document.querySelector('.block-minicart');
+            let currentHeight = element.getBoundingClientRect().height;
+            let stopMovingCounter = 0;
 
-          await new Promise((resolve) => {
-            const stoppedMoving = function() {
-                if (element.getAttribute('style') !== 'display: block;') {
+            await new Promise((resolve) => {
+                const stoppedMoving = function () {
+                    if (element.getAttribute('style') !== 'display: block;') {
+                        setTimeout(stoppedMoving, 20);
+                    }
+
+                    if (element.getBoundingClientRect().height > 0 && currentHeight === element.getBoundingClientRect().height) {
+                        stopMovingCounter++;
+                    } else {
+                        stopMovingCounter = 0;
+                        currentHeight = element.getBoundingClientRect().height
+                    }
+
+                    if (stopMovingCounter === 10) {
+                        console.log('resolve');
+                        resolve();
+                    }
+
                     setTimeout(stoppedMoving, 20);
-                }
-    
-                if(element.getBoundingClientRect().height > 0 && currentHeight === element.getBoundingClientRect().height) {
-                    stopMovingCounter++;
-                } else {
-                    stopMovingCounter = 0;
-                    currentHeight = element.getBoundingClientRect().height
-                }
-                
-                if(stopMovingCounter === 10) {
-                    resolve();
-                }
-    
-                setTimeout(stoppedMoving, 20);
-            };
-            stoppedMoving()
-        });
+                };
+                stoppedMoving()
+            });
 
-          return true;
+            return true;
         });
-
         await page.click('#btn-minicart-close');
         await cartIsOnePromise;
         await addToCartButtons[1].click();
@@ -99,71 +99,70 @@ describe('Packpub search Test', () => {
     });
 
 
-    it('Should update the cart number (with observer)', async() => {
+    it('Should update the cart number (with listeners)', async () => {
         await page.goto('https://www.packtpub.com/tech/javascript/');
-        const cookieLink = await page.waitForSelector('.accept_all', { timeout: 1000}).catch(e => e);
+        const cookieLink = await page.waitForSelector('.accept_all', { timeout: 1000 }).catch(e => e);
 
         if (cookieLink) {
-          await cookieLink.click().catch({});
+            await cookieLink.click().catch(e => e);
         }
 
         await Promise.all([
-          page.waitForSelector('.counter.qty.empty'),
-          page.waitForSelector('.add-to-cart')
+            page.waitForSelector('.counter.qty.empty'),
+            page.waitForSelector('.add-to-cart')
         ]);
 
-        const reachedToTwo = new Promise(resolve => {
+        const reachedToTwo = new Promise((resolve) => {
             page.exposeFunction('notifyCartChange', i => {
-                if (i === '2') {
+                if (i === '2')
                     resolve();
-                }
             })
         });
-
         await page.evaluate(() => {
             let observer = new MutationObserver(list => notifyCartChange(list[0].target.nodeValue));
             observer.observe(
                 document.querySelector('.counter.qty .counter-number'),
                 {
-                    characterData: true, 
-                    attributes: false, 
-                    childList: false, 
+                    characterData: true,
+                    attributes: false,
+                    childList: false,
                     subtree: true
                 });
         });
+
         const addToCartButtons = await page.$$('.add-to-cart');
         await addToCartButtons[0].click();
 
         await page.waitForFunction(async () => {
-          const element = document.querySelector('.block-minicart');
-          let currentHeight = element.getBoundingClientRect().height;
-          let stopMovingCounter = 0;
+            const element = document.querySelector('.block-minicart');
+            let currentHeight = element.getBoundingClientRect().height;
+            let stopMovingCounter = 0;
 
-          await new Promise((resolve) => {
-            const stoppedMoving = function() {
-                if (element.getAttribute('style') !== 'display: block;') {
+            await new Promise((resolve) => {
+                const stoppedMoving = function () {
+                    if (element.getAttribute('style') !== 'display: block;') {
+                        setTimeout(stoppedMoving, 20);
+                    }
+
+                    if (element.getBoundingClientRect().height > 0 && currentHeight === element.getBoundingClientRect().height) {
+                        stopMovingCounter++;
+                    } else {
+                        stopMovingCounter = 0;
+                        currentHeight = element.getBoundingClientRect().height
+                    }
+
+                    if (stopMovingCounter === 10) {
+                        console.log('resolve');
+                        resolve();
+                    }
+
                     setTimeout(stoppedMoving, 20);
-                }
-    
-                if(element.getBoundingClientRect().height > 0 && currentHeight === element.getBoundingClientRect().height) {
-                    stopMovingCounter++;
-                } else {
-                    stopMovingCounter = 0;
-                    currentHeight = element.getBoundingClientRect().height
-                }
-                
-                if(stopMovingCounter === 10) {
-                    resolve();
-                }
-    
-                setTimeout(stoppedMoving, 20);
-            };
-            stoppedMoving()
-        });
+                };
+                stoppedMoving()
+            });
 
-          return true;
+            return true;
         });
-
         await page.click('#btn-minicart-close');
         await addToCartButtons[1].click();
         await reachedToTwo;
